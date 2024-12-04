36331c1bfe5b62e6d330399be5460397f678fb85
Alexander Engelhardt <alexander.w.engelhardt@gmail.com>
2024-12-04 21:49:01 +0100

    Adjust neotree width

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -6,6 +6,16 @@ return {
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-  }
+  },
+  config = function()
+    local neotree = require("neo-tree")
+    neotree.setup({
+      window = {
+        width = 30,
+      }
+    })
+
+
+  end
 }
 
